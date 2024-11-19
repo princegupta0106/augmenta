2a751170583ed812fd306b12c7445d92301578ab
princegupta0106 <princeguptapg0106@gmail.com>
2024-11-20 01:41:10 +0530

    Your commit message

--- src/components/NewPage2.jsx
+++ src/components/NewPage2.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import asia from '../assets/images/asia.jpeg';
+import React from "react";
+import asia from "../assets/images/Asia.jpeg";
 const NewPage2 = () => {
   return (
     <div class="flex justify-center bg-[#b23d29]">
-      <img src={asia} alt=""  class="h-[200px] md:h-[300px] "/>
+      <img
+        src={asia}
+        alt=""
+        class="h-[200px] md:h-[300px] "
+      />
     </div>
-  )
-}
+  );
+};
 
-export default NewPage2
+export default NewPage2;
